038f6de20dfa9d848e03dd3cbbc20c30d8c27b54
Prasanna Ranganathan <prasanna.ranganathan@prasanna-ranganathan-C02F70EWMD6V.local>
2021-12-26 20:19:03 +0530

    A Counter Example using Typescript & SOlidity

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+import "@nomiclabs/hardhat-waffle"; 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+      hardhat: {
+        chainId: 1337
+      }
+  }
+};
